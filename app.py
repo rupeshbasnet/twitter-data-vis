105da1e8213943e5fb3ade475fe1be853510b8eb
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-09-25 17:19:52 -0400

    Adding percentage calculator

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from textblob import TextBlob
 import sys, tweepy
 import matplotlib.pyplot as plt
 from passwords import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+import csv
 
 TWITTER_KEY = API_KEY
 TWITTER_KEY_SECRET = API_SECRET_KEY
@@ -21,31 +22,47 @@ api = tweepy.API(auth)
 def index():
     return render_template('index.html')
 
+
+def percentageCalc(section, total):
+    return 100 * float(section)/float(total)
+
+
+def tweetsIterator(tweets):
+    positiveCount = 0
+    negativeCount = 0
+    neutralCount = 0
+    sentiments = 0
+    for tweet in tweets:
+        textAnalysis = TextBlob(tweet.text)
+        print (tweet.text)
+        print()
+        sentiments += textAnalysis.sentiment.polarity
+        if (textAnalysis.sentiment.polarity == 0):
+            neutralCount += 1
+        elif (textAnalysis.sentiment.polarity < 0.00):
+            negativeCount += 1
+        elif (textAnalysis.sentiment.polarity > 0.00):
+            positiveCount += 1
+    return positiveCount, negativeCount, neutralCount
+
+
 @app.route('/query', methods=['POST'])
 def query():
     if request.method == 'POST':
         userInput = request.form['userInput']
         # Once we get the user input we will process it with the Twitter
         # Sentiment Analysis Pipeline and display the results/Visualization
-        tweets = tweepy.Cursor(api.search, q=userInput, lang="English").items(30)
-
-        positiveCount = 0
-        negativeCount = 0
-        neutralCount = 0
-        sentiments = 0
-        for tweet in tweets:
-            textAnalysis = TextBlob(tweet.text)
-            sentiments += textAnalysis.sentiment.polarity
-            if (textAnalysis.sentiment.polarity == 0):
-                neutralCount += 1
-            elif (textAnalysis.sentiment.polarity < 0.00):
-                negativeCount += 1
-            elif (textAnalysis.sentiment.polarity > 0.00):
-                positiveCount += 1
+        totalNumberOfTweets = 100
+        tweets = tweepy.Cursor(api.search, q=userInput, lang="English").items(totalNumberOfTweets)
+        positiveCount, negativeCount, neutralCount = tweetsIterator(tweets)
+        positiveCount = format(percentageCalc(positiveCount, totalNumberOfTweets), '.2f')
+        negativeCount = format(percentageCalc(negativeCount, totalNumberOfTweets), '.2f')
+        neutralCount = format(percentageCalc(neutralCount, totalNumberOfTweets), '.2f')
 
     return render_template('index.html', neutralCount = neutralCount,
                                          negativeCount = negativeCount,
-                                         positiveCount = positiveCount)
+                                         positiveCount = positiveCount,
+                                         totalNumberOfTweets = totalNumberOfTweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
