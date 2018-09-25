8a656075b7a651fa3a103dee74a4b3e0bcbde6c9
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-09-24 21:09:29 -0400

    Added form elements for testing user query

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/query')
+def query():
     return render_template('index.html')
 
 if __name__ == '__main__':
