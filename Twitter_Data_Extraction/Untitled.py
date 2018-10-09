0d4d7b8d5091581d440dc0afa9af98daeb88c60d
Chhewang Sherpa <chhewangsrp@gmail.com>
2018-10-09 13:50:36 -0400

    New Changes

--- Twitter_Data_Extraction/Untitled.py
+++ Twitter_Data_Extraction/Untitled.py
@@ -1,9 +1,23 @@
-import json
-import csv
-import pandas as pd
 import nltk
+import numpy as np,pandas as pd
+import matplotlib.pyplot as plt 
+import nltk.data
+import re
+from nltk.stem.porter import *
+pd.options.mode.chained_assignment = None
+
+df = pd.read_csv("clean_data.csv")
+
+# step 1 stop word removal
+short_data = df.head()
 from nltk.corpus import stopwords
+stop = stopwords.words("english")
 
-data = pd.read_csv("clean_data.csv")
+print(short_data['text'])
+print('-------Remove Stop Word--------')
+short_data['Step1_SentimentText'] = short_data['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+print(short_data['Step1_SentimentText'])
+#clean = short_data['Step1_SentimentText']
+#clean = re.sub('@[^\s]+','',clean)
 
-string_type = data.astype(str)
+
