13d8747526f15961404c9188a38e22a21dfc755a
Rupesh Basnet <rupeshbasnet93@gmail.com>
2018-10-16 23:44:39 -0400

    manually adding the colors

--- flask_app/uStates.js
+++ flask_app/uStates.js
@@ -209,7 +209,6 @@
     uStates.draw = function(id, data, toolTip) {
         function mouseOver(d) {
             d3.select("#tooltip").transition().duration(200).style("opacity", .9);
-
             d3.select("#tooltip").html(toolTip(d.n, data[d.id])).style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY - 28) + "px");
         }
 
@@ -219,8 +218,8 @@
 
         d3.select(id).selectAll(".state").data(uStatePaths).enter().append("path").attr("class", "state").attr("d", function(d) {
             return d.d;
-        }).style("fill", "#ffffcc").
-        on("mouseover", mouseOver).on("mouseout", mouseOut);
+        }).style("fill", function(d){ return data[d.id].color; })
+        .on("mouseover", mouseOver).on("mouseout", mouseOut);
     }
     this.uStates = uStates;
 }
