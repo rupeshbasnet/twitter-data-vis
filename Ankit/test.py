2db552d89b24d658282ae9d78f6f03a297a35a8a
Ankit Shrestha <ankieet8030@gmail.com>
2018-10-24 11:50:08 -0400

    Created a website prototype

--- Ankit/test.py
+++ Ankit/test.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 #import Flask and jsonify from flask
-from flask import Flask,jsonify
+from flask import Flask,jsonify,render_template,url_for
+import json
+#from stock_scraper import get_data
 #create an app using __name__ variable when the python
 #program in interpreted.
 app = Flask(__name__)
+
 
 #import engine and sessionmaker from sqlalchemy to connect
 #to the database and create sessions.
@@ -23,22 +26,48 @@ Base.metadata.bind=engine
 session = scoped_session(sessionmaker(bind=engine))
 #session = DBSession()
 
+@app.route('/twitterData/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/twitterData/visualizations')
+def visualizations():
+    return render_template('visualizations.html')
+
+
+
+@app.route('/twitterData/UsMap')
+def UsMap():
+    return render_template('UsMap.html')
+
+@app.route('/twitterData/forced')
+def forced():
+    return render_template('forced.html')
+
+@app.route('/twitterData/WordCloud')
+def WordCloud():
+    return render_template('worldcloud.html')
 #creating URL route for WITBRAGDAY 
 @app.route('/twitterData/WITBRAGDAY')
 def twitterDataJSON():
-    #Get all the data from TwitterData and save it in Data.
-    Data = session.query(TwitterData).all()
-    #Using Jsonify function from flask to access each piece of
-    #information from Data and use serialize property defined
-    # in database_setup.py to create JSON objects.
-    return jsonify(Tweets = [i.serialize for i in Data] )
+	#Get all the data from TwitterData and save it in Data.
+	Data = session.query(TwitterData).all()
+	#Using Jsonify function from flask to access each piece of
+	#information from Data and use serialize property defined
+	# in database_setup.py to create JSON objects.
+	return jsonify(Tweets = [i.serialize for i in Data] )
 #creating URL route for WITBRAGDAY when tweets are filtered by id.
 @app.route('/twitterData/<int:id>/JSON')
 def twitterDataPerIDJSON(id):
-    #Get information on each tweet filtered by id and save it into Data.
+	#Get information on each tweet filtered by id and save it into Data.
     Data = session.query(TwitterData).filter_by(id = id).all()
     #Using jsonify function from flask to access the tweets from Data and use
     #serialize property in database_setup.py to create JSON objects.
+    return jsonify(Tweets = [i.serialize for i in Data])
+
+@app.route('/twitterData/<string:Polarity>/WITBRAGDAY')
+def twitterDataPolarity(Polarity):
+    Data = session.query(TwitterData).filter_by(Polarity = Polarity).all()
     return jsonify(Tweets = [i.serialize for i in Data])
 #if the program is interpreted using python, do the following.
 if __name__ == '__main__':
